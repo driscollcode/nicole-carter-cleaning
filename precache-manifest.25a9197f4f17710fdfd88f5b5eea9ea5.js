59033dce84dd7be3f57e3c0588b8b78a25c4c714
John Driscoll <jsecure@Mac-Studio.local>
2022-04-02 01:08:58 +0100

    Updates

---
+++ precache-manifest.25a9197f4f17710fdfd88f5b5eea9ea5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "40f54b9a80d35f7c102d",
+    "url": "/static/js/main.40f54b9a.chunk.js"
+  },
+  {
+    "revision": "e8e9e3f794d1f4f98c89",
+    "url": "/static/js/1.e8e9e3f7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9009873dcfbf5859c0bddc7e8a5acdad",
+    "url": "/index.html"
+  }
+];
